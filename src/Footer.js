9a70e43d5e675cf80fc522f922ad0d7e82460afb
Kevin Leon <50714402+kevinleon01@users.noreply.github.com>
2022-09-04 19:55:39 -0500

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="footer">
-            <footer> <div className="footerText">2022. Kevin León for Imanyco</div></footer>
+            <footer> <div className="footerText">2022. Kevin León</div></footer>
         </div>
     );
 }
